203669772b6d4ecfcf183d65acf702f98e412e8b
kppetrick <60531603+kppetrick@users.noreply.github.com>
2021-02-15 20:17:53 -0500

    Add files via upload

---
+++ src/main/java/Practice.java
@@ -0,0 +1,13 @@
+public class Practice {
+    public static String maskify(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.length() > 4 && i < str.length() - 4) {
+                answer += '#';
+                } else {
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
