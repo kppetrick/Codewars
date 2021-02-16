381386869ac2d23521cc14906f1020e4bb2f7950
kppetrick <kppetrick@gmail.com>
2021-02-16 15:42:01 -0500

    DRoot

---
+++ src/main/java/DRoot.java
@@ -0,0 +1,15 @@
+public class DRoot {
+    public static int digital_root(int n) {
+        int root = 0;
+
+        while (n > 0 || root > 9) {
+            if (n == 0) {
+                n = root;
+                root = 0;
+            }
+                root += n % 10;
+                n /= 10;
+            }
+        return root;
+    }
+}
--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -11,3 +11,4 @@ public class Practice {
         return answer;
     }
 }
+
