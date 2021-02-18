21c4e6a574d6b1d171b8b0afc44f11307b722198
kppetrick <kppetrick@gmail.com>
2021-02-18 14:44:14 -0500

    refactor longestConsec

--- src/main/java/LongestConsec.java
+++ src/main/java/LongestConsec.java
@@ -3,34 +3,17 @@ class LongestConsec {
     public static String longestConsec(String[] strarr, int k) {
         String answer = "";
         String current = "";
-
-        if (k == 1) {
-            for (int i = 0; i < strarr.length; i++) {
-                current = "";
-                current += strarr[i];
-                if (current.length() > answer.length()) {
-                    answer = current; }
-                }
-                return answer;
-        } if (k <= 0) {
-            return answer;
-        }
-        else {
-            for (int i = 0; i < strarr.length - (k-1); i++) {
-                current += strarr[i];
-                for (int j = 1; j < k; j++) {
-                    current += strarr[i + j];
-                    if (j == k - 1) {
-                        if (current.length() > answer.length()) {
-                            answer = current;
-                            current = "";
-                        } else {
-                            current = "";
-                        }
+        for (int i = 0; i < strarr.length - (k-1); i++) {
+            for (int j = 0; j < k; j++) {
+                current += strarr[i + j];
+                if (j == k - 1) {
+                    if (current.length() > answer.length()) {
+                        answer = current;
                     }
+                    current = "";
                 }
             }
-            return answer;
         }
+        return answer;
     }
 }
