83146ba20b5138d4a02f3475ed314b11317de3bd
kppetrick <kppetrick@gmail.com>
2021-02-23 15:05:27 -0500

    fixed ASum

--- src/main/java/Kyu6/ASum.java
+++ src/main/java/Kyu6/ASum.java
@@ -2,15 +2,7 @@ package Kyu6;
 
 public class ASum {
     public static long findNb(long m) {
-        int answer = 0;
-        answer = (int) Math.sqrt((Math.sqrt(m)) * 2);
-        long test = 0;
-        for (int i = 1; i <= answer; i++) {
-            test += Math.pow(i, 3);
-        }
-        if (test == m) {
-            return answer;
-        }
-        return -1;
+        long ex=(long)Math.sqrt((long) Math.sqrt(m)*2);
+        if((ex*(ex+1)/2)*(ex*(ex+1)/2)==m) return ex; else return -1;
     }
 }
