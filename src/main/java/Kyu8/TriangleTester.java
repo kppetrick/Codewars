b85f8eddc91d93d26f165b6ba8e2c93ae25caead
kppetrick <kppetrick@gmail.com>
2021-02-27 11:10:47 -0500

    triangles

---
+++ src/main/java/Kyu8/TriangleTester.java
@@ -0,0 +1,10 @@
+package Kyu8;
+
+class TriangleTester {
+    public static boolean isTriangle(int a, int b, int c) {
+        if (a + b > c && a + c > b && b + c > a) {
+            return true;
+        }
+        return false;
+    }
+}
